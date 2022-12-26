c9a5b335b427efdf3756a56114151cfa016e151c
Qz <qzcurious@gmail.com>
2022-12-26 23:36:12 +0800

    cool, that works

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,12 @@
-import { Alert } from "ui";
+import Alert from "ui/Alert";
+import { Button } from "ui/Button";
 
 export default function Web() {
   return (
     <div>
       <h1>Web</h1>
       <Alert />
+      <Button></Button>
     </div>
   );
 }
